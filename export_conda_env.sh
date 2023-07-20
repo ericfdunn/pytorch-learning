27bfe6761be772375f1550e2bfb26003d38a7234
ericfdunn <20568610+ericfdunn@users.noreply.github.com>
2023-07-20 13:14:28 -0700

    fixed export_conda_env.sh to remove prefix path

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -9,8 +9,8 @@ mkdir -p ./conda-env
 # Navigate to the directory where you want to put the environment.yml
 cd ./conda-env
 
-# Export the environment
-conda env export > environment.yml
+# Export the environment excluding the prefix line
+conda env export | grep -v "^prefix: " > environment.yml
 
 # Add a success message
 echo "Exported the deeplearning environment to environment.yml"
